25fef0bed8b7898b447673881d8ba929c389ba45
Jayesh Junghare <jayeshjunghare2001.com>
2023-08-22 18:36:09 +0530

    First Commit

---
+++ app/src/main/java/com/example/memorymaster/utils/Constants.kt
@@ -0,0 +1,19 @@
+package com.example.memorymaster.utils
+
+import com.example.memorymaster.R
+
+val DEFAULT_ICONS = listOf(
+            R.drawable.ic_face,
+            R.drawable.ic_card_gift,
+            R.drawable.ic_flag,
+            R.drawable.ic_flight,
+            R.drawable.ic_florist,
+            R.drawable.ic_heart_broken,
+            R.drawable.ic_home,
+            R.drawable.ic_pentagon,
+            R.drawable.ic_school,
+            R.drawable.ic_send,
+            R.drawable.ic_star,
+            R.drawable.ic_work
+
+)
